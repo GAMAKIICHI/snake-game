73f99f4f11d01c00cc39174dd0c7390ed1d2d7fd
GAMAKIICHI <efabio05@icloud.com>
2022-09-18 13:55:33 -0700

    Added display class. THis class creates and displays a window

--- snake/display.py
+++ snake/display.py
@@ -1,5 +1,28 @@
 import pygame
 
-#pygame.init()
-size = width,height = 550,500
-display = pygame.display.set_mode(size)
+class display:
+    def __init__(self, width:int, height:int, title:str):
+        pygame.init()
+
+        (self.width, self.height) = (width, height)
+        self.title = title
+
+        pygame.display.set_caption(title)
+    
+    #initialize a window to display
+    def setDisplay(self):
+        self.display = pygame.display.set_mode((self.width, self.height))
+    
+    #Renders window
+    def render(self):
+        self.isRender = True
+        self.setDisplay()
+
+        while self.isRender:
+            pygame.display.flip()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.isRender = False
+
+# Example - 
+# display(500,550, "Test").render()
