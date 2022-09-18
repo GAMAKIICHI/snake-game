4a14558a2ad5e85637f7397de7264c39f738da76
GAMAKIICHI <efabio05@icloud.com>
2022-09-18 14:00:44 -0700

    Added comments to clarify code

--- snake/display.py
+++ snake/display.py
@@ -1,7 +1,11 @@
 import pygame
 
+#This code creates and displays a window based on size given
+# Example - 
+# display(500,550, "Test").render()
+
 class display:
-    def __init__(self, width:int, height:int, title:str):
+    def __init__(self, width:int, height:int, title:str = "Title"):
         pygame.init()
 
         (self.width, self.height) = (width, height)
@@ -18,11 +22,9 @@ class display:
         self.isRender = True
         self.setDisplay()
 
+        #Required to close window
         while self.isRender:
             pygame.display.flip()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.isRender = False
-
-# Example - 
-# display(500,550, "Test").render()
