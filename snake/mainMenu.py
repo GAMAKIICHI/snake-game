2dd57dee3d968c9c21940d77902873b2d4ee3342
GAMAKIICHI <efabio05@icloud.com>
2022-09-18 19:13:27 -0700

    created the text for the start button and leaderboard button

--- snake/mainMenu.py
+++ snake/mainMenu.py
@@ -5,6 +5,7 @@ pygame.font.init()
 
 width = 550
 height = 500
+fontColor = "green"
 
 window = display(width, height, "Snake")
 menuScene = window.setDisplay()
@@ -13,9 +14,12 @@ def centerText(text, textHeight):
     alignText = text.get_rect(center = (width/2,height/textHeight))
     return alignText
 
-title = window.setFont().render("Snake", True, "green")
+titleTxt = window.setFont(80).render("Snake", True, fontColor)
+startBtn = window.setFont(48).render("Start", True, fontColor)
+leaderboardBtn = window.setFont(48).render("Start", True, fontColor)
 
 menuScene.blit(title,centerText(title,2.5))
+menuScene.blit(startBtn, centerText(startBtn, 1.8))
 window.render()
 
 
