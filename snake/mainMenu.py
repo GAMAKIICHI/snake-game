be1032f24643a7c0c7a02761e3446f54791342ec
GAMAKIICHI <efabio05@icloud.com>
2022-09-18 19:20:48 -0700

    added comments to organize code better and added start/high score buttons

--- snake/mainMenu.py
+++ snake/mainMenu.py
@@ -14,13 +14,19 @@ def centerText(text, textHeight):
     alignText = text.get_rect(center = (width/2,height/textHeight))
     return alignText
 
+#Title
 titleTxt = window.setFont(80).render("Snake", True, fontColor)
+
+#Buttons
 startBtn = window.setFont(48).render("Start", True, fontColor)
 hiScoreBtn = window.setFont(48).render("Top Scores", True, fontColor)
 
+#Render texts
 menuScene.blit(titleTxt, centerText(titleTxt,2.5))
 menuScene.blit(startBtn, centerText(startBtn, 1.8))
 menuScene.blit(hiScoreBtn, centerText(hiScoreBtn, 1.5))
+
+
 window.render()
 
 
